9eef7f093ff0a6b74f5b548cd78f45fb9d837df8
Eric Young <Eric.Young@snow.edu>
2022-03-25 12:11:13 -0600

    got the client working, should only require caching implementation inside of the server

--- FactorsClient/Program.cs
+++ FactorsClient/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        int PORT = 6544;
+        UdpClient udpClient = new UdpClient();
+
+        while (true)
+        {
+            string request = Console.ReadLine();
+            var data = Encoding.UTF8.GetBytes(request);
+            udpClient.Send(data, data.Length, "255.255.255.255", PORT);
+            var from = new IPEndPoint(0, 0);
+            byte[] recvBuffer = udpClient.Receive(ref from);
+            string message = Encoding.UTF8.GetString(recvBuffer);
+            Console.WriteLine(message);
+        }
+     
+    }
+}
+
