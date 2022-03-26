92864987c2b242446dcf78d64ef6b96038595842
Chance Young <Chanceyoungwebdev@gmai.com>
2022-03-25 19:48:06 -0600

    completed lab with cache

--- FactorsServer/Program.cs
+++ FactorsServer/Program.cs
@@ -4,24 +4,56 @@ using System.Text;
 
 class Server
 {
+    static Dictionary<string, string> cache = new Dictionary<string, string>();
+
+    public static string checkCache(string requestString)
+    {
+        lock (cache)
+        {
+            if (cache.ContainsKey(requestString))
+            {
+                return cache[requestString];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+    }
     static void Main()
     {
         int requestsPort = 6544;
         UdpClient requestsClient = new UdpClient(requestsPort);
         UTF8Encoding encoding = new UTF8Encoding();
-
         while (true)
         {
             IPEndPoint requester = new IPEndPoint(0, 0);
             byte[] requestData = requestsClient.Receive(ref requester);
+
             // start a thread to respond
             Task.Run(() =>
             {
                 string requestString = encoding.GetString(requestData);
-                long request = long.Parse(requestString);
-                List<long> answer = GetPrimeFactors(request);
-                string response = String.Join(',', answer.Select(p => p.ToString()));
-                byte[] responseData = encoding.GetBytes(response);
+                string response;
+                byte[] responseData;
+                string cacheCheck = checkCache(requestString);
+                if (cacheCheck != string.Empty)
+                {
+                    response = cacheCheck;
+                }
+                else
+                {
+                    long request = long.Parse(requestString);
+                    List<long> answer = GetPrimeFactors(request);
+                    response = String.Join(',', answer.Select(p => p.ToString()));
+                    lock (cache)
+                    {
+                        cache.Add(requestString, response);
+                    }
+                }
+
+                responseData = encoding.GetBytes(response);
                 UdpClient toClient = new UdpClient();
                 toClient.Send(responseData, responseData.Length, requester);
             });
